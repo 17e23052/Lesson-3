9272d79f62ddb5801b05457fc3d01f875669e322
Mathman <17e23052@marling.gloucs.sch.uk>
2020-09-15 09:59:38 +0000

    My story v1.2

--- main.py
+++ main.py
@@ -10,3 +10,15 @@ adjective = "hastily"
 place = "his master"
 area = "the field"
 print(f"The {noun} {verb} {adjective} to {place} on {area}")
+noun = "robot"
+verb = "got on"
+vehicle = "the flying bus"
+verb2 = "sat down"
+print(f"The {noun} {verb} {vehicle} and {verb2}")
+noun = "rat"
+verb = "scuttled"
+noun2 = "skyscraper"
+verb2 = "looked"
+adjective = "giant"
+noun3 = "window"
+print(f"The {noun} {verb} up the {noun2} and {verb2} thorugh the {adjective} {noun3}")
