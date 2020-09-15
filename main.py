2cfc19e54550bdf3ad24764a499b1abec52f4cec
Mathman <17e23052@marling.gloucs.sch.uk>
2020-09-15 09:10:40 +0000

    Example code

---
+++ main.py
@@ -0,0 +1,8 @@
+noun = "car"
+adverb = "gently"
+adjective = "loud"
+print(f"The {noun} was {adjective} when it {adverb} went to school")
+noun = "zebra"
+adverb = "aggresively"
+adjective = "giant"
+print(f"The {noun} was {adjective} when it {adverb} went to school")
