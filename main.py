740c09d93e0bed860f803c3462746075e9fa1bcc
Mathman <17e23052@marling.gloucs.sch.uk>
2020-09-16 09:31:00 +0000

    My story with inputs

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-noun = "alien"
-verb = "jogged"
-adjective = "cheerfully"
-place = "school"
-time = "10 minutes"
-print(f"The {noun} {verb} {adjective} to {place} in {time}")
-noun = "mutant dog"
-verb = "ran"
-adjective = "hastily"
-place = "his master"
-area = "the field"
-print(f"The {noun} {verb} {adjective} to {place} on {area}")
-noun = "robot"
-verb = "got on"
-vehicle = "the flying bus"
-verb2 = "sat down"
-print(f"The {noun} {verb} {vehicle} and {verb2}")
-noun = "rat"
-verb = "scuttled"
-noun2 = "skyscraper"
-verb2 = "looked"
-adjective = "giant"
-noun3 = "window"
-print(f"The {noun} {verb} up the {noun2} and {verb2} thorugh the {adjective} {noun3}")
+noun = input("Insert noun: ")
+verb = input("Insert verb: ")
+adjective = input("Insert adjective: ")
+place = input("Insert place: ")
+time = int(input("Insert time in minutes: "))
+print(f"The {noun} {verb} {adjective} to {place} in {time} minutes")
